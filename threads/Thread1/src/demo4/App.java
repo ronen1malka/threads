3cff44d8208a5ab247c2e2bd11bd1aacb599c12f
ronenm <ronenm@ronenm1-hrdt.fundtech.isr>
2017-02-12 18:00:16 +0200

    demo4

---
+++ threads/Thread1/src/demo4/App.java
@@ -0,0 +1,39 @@
+package demo4;
+
+import java.util.Scanner;
+
+class Processor extends Thread {
+
+	private volatile boolean running = true;
+
+	@Override
+	public void run() {
+		while (running) {
+
+			System.out.println("Hello");
+
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void shutDown() {
+		this.running = false;
+	}
+}
+
+public class App {
+	public static void main(String[] args) {
+		Processor proc = new Processor();
+		proc.start();
+		
+		Scanner scan = new Scanner(System.in);
+		scan.nextLine();
+		proc.shutDown();
+		
+	}
+}
